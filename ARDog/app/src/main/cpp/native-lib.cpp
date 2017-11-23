8648fb0ebaeb809e82533c714c58f774c1452e0c
kangaro0 <johannes_herbert@yahoo.de>
2017-11-23 11:13:22 +0100

    added java-classes

--- ARDog/app/src/main/cpp/native-lib.cpp
+++ ARDog/app/src/main/cpp/native-lib.cpp
@@ -19,3 +19,55 @@ jint JNI_OnLoad(JavaVM* vm, void*) {
     app.SetJavaVM(vm);
     return JNI_VERSION_1_6;
 }
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onCreate(
+        JNIEnv* env, jobject, jobject activity, int display_orientation) {
+    app.onCreate(env, activity, display_orientation);
+}
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onTangoServiceConnected(
+        JNIEnv* env, jobject, jobject iBinder) {
+    app.onTangoServiceConnected(env, iBinder);
+}
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onPause(
+        JNIEnv*, jobject) {
+    app.onPause();
+}
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onDestroy(
+        JNIEnv*, jobject) {
+    app.onDestroy();
+}
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onGlSurfaceCreated(
+        JNIEnv* env, jobject, jobject j_asset_manager) {
+    AAssetManager* aasset_manager = AAssetManager_fromJava(env, j_asset_manager);
+    app.onSurfaceCreated(aasset_manager);
+}
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onGlSurfaceChanged(
+        JNIEnv*, jobject, jint width, jint height) {
+    app.onSurfaceChanged(width, height);
+}
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onGlSurfaceDrawFrame(
+        JNIEnv*, jobject) {
+    app.onDrawFrame();}
+
+JNIEXPORT void JNICALL
+Java_com_projecttango_examples_cpp_augmentedreality_TangoJNINative_onConfigurationChanged(
+        JNIEnv*, jobject, int display_orientation) {
+    app.onDeviceRotationChanged(display_orientation);
+}
+
+#ifdef __cplusplus
+}
+#endif
--- ARDog/app/src/main/java/de/fhws/mobcom/ardog/JNINative.java
+++ ARDog/app/src/main/java/de/fhws/mobcom/ardog/JNINative.java
@@ -9,8 +9,30 @@ import android.content.res.AssetManager;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.projecttango.examples.cpp.util.TangoInitializationHelper;
+
 public class JNINative {
     static {
-
+        if( TangoInitializationHelper.loadTangoSharedLibrary() ==
+                TangoInitializationHelper.ARCH_ERROR ){
+            Log.e("JNINative", "ERRROR! Unable to load libtango_client_api.so!");
+        }
+        System.loadLibrary( "native-lib");
     }
+
+    public static native void onCreate( Activity callerActivity, int activityOrientation );
+
+    public static native void onTangoServiceConnected( IBinder nativeTangoServiceBinder );
+
+    public static native void onPause();
+
+    public static native void onDestroy();
+
+    public static native void onGlSurfaceCreated( AssetManager assetManager );
+
+    public static native void onGlSurfaceChanges( int width, int height );
+
+    public static native void onGlSurfaceDrawFrame();
+
+    public static native void onConfigurationChanged( int displayOrientation );
 }
